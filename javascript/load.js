ac99e16f3fd63b0fdfd65e29694590380f262e82
saeed labtop <eng.saeedsayed@gmail.com>
2025-06-11 21:42:25 +0300

    first commit

---
+++ javascript/load.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  'use strict';
+  var windoh = $(window).height();
+  $('body').height(windoh);
+
+  $(".component-stert  .component--intro ").fadeOut(1000, function () {
+    $(this).parent().fadeOut(1000, function () {
+      $("body").css("overflow", "auto");
+      $(this).remove();
+      $('body').height("100%");
+    });
+  }
+  );
+})

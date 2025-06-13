898960fae92d4f100e1227534e62923ebeb7d26e
saeed <eng.saeedsayed@gmail.com>
2025-06-13 16:21:34 +0300

    Refactor accounting service page and related styles
    - Updated AOS CSS and JS to version 2.3.1.
    - Replaced Bootstrap CSS with the correct version and updated integrity hash.
    - Introduced odometer.js for client and experience counters with new styling.
    - Enhanced header with scroll effect and responsive design.
    - Revamped footer structure and styling for better layout and responsiveness.
    - Added phone banner for contact information with improved styling.
    - Cleaned up unused CSS rules and organized styles for better maintainability.
    - Implemented JavaScript for header activation on scroll and odometer functionality.
    - Fixed various typos and improved overall code readability.

---
+++ javascript/main.js
@@ -0,0 +1,31 @@
+
+var header = document.getElementById("header");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+        header.classList.add("active");
+    } else {
+        header.classList.remove("active");
+    }
+});
+
+const subsOdometer = document.querySelectorAll('.rs-count')
+if (subsOdometer.length > 0 && typeof Odometer !== 'undefined') {
+    subsOdometer.forEach((el) => {
+        const odometer = new Odometer({
+            el: el,
+            // value: el.dataset.count,
+            // format: '(,ddd)',
+            // theme: 'default',
+            duration: 2000,
+        });
+        // odometer.render();
+        // odometer.update(el.dataset.count);
+        el.innerHTML = el.dataset.count;
+    });
+}
+AOS.init({
+    offset: 120,
+    duration: 500,
+    once: false,
+});

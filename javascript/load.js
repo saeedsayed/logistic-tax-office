12050022f429596ef012dd82874b6625328e9bde
saeed pc <saeidsaued@gmail.com>
2025-06-13 21:37:36 +0300

    fix: update loading animation timing and enhance header styles for better UX

--- javascript/load.js
+++ javascript/load.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   var windowHeight = $(window).height();
   $('body').height(windowHeight);
 
-  $(".main-loader  .pulse-logo").fadeOut(1000, function () {
+  $(".main-loader  .pulse-logo").fadeOut(500, function () {
     $(this).parent().fadeOut(1000, function () {
       $("body").css("overflow", "auto");
       $(this).remove();

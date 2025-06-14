7119a6a7d0adc66a26115fb9b9094edb26c92101
saeed pc <saeidsaued@gmail.com>
2025-06-14 13:36:36 +0300

    Refactor header and footer styles, enhance responsive design, and improve sidebar functionality
    - Removed unnecessary inline styles and improved structure in index.html
    - Updated JavaScript to manage sidebar and dropdown interactions more effectively
    - Consolidated CSS by importing main.css into various stylesheets
    - Created separate header.css and footer.css files for better organization
    - Enhanced mobile navigation experience with improved dropdown and sidebar functionality
    - Added animations for smoother transitions in header and sidebar
    - Cleaned up unused CSS rules and optimized existing styles for better performance

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,9 @@
 
-var header = document.getElementById("header");
+AOS.init({
+    offset: 120,
+    duration: 1000,
+    once: false,
+});
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > 200) {
@@ -24,8 +28,36 @@ if (subsOdometer.length > 0 && typeof Odometer !== 'undefined') {
         el.innerHTML = el.dataset.count;
     });
 }
-AOS.init({
-    offset: 120,
-    duration: 1000,
-    once: false,
+
+// add background  header
+let header = document.getElementById("header");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
+
+// add class list active to header | responsive And select to elements
+
+let dropdownMenu = document.getElementById("dropdown-menu");
+let dropdownBtn = document.getElementById("dropdown-btn");
+let sidebar = document.getElementById("sidebar");
+let closeSidebarBtn = document.getElementById("closeSidebar");
+let openSidebarBtn = document.getElementById("openSidebar");
+
+
+dropdownBtn.addEventListener("click", () => {
+    dropdownMenu.classList.toggle("active");
+});
+
+// Open And Close Header
+
+closeSidebarBtn.addEventListener("click", () => {
+    sidebar.classList.remove("active");
+});
+
+openSidebarBtn.addEventListener("click", () => {
+    sidebar.classList.add("active");
 });
